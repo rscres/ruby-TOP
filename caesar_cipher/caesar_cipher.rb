6c1bf4db5a8e41e8b8da060db3e9accff63d526b
rscres <renatocrescenti@gmail.com>
2022-08-24 13:35:08 -0300

    added case statement for numbers

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -7,6 +7,8 @@ def caesar_cipher(message, cipher_key = 3)
       ciphertext = (((code - 65) + cipher_key) % 26) + 65
     when code.between?(97, 122)
       ciphertext = (((code - 97) + cipher_key) % 26) + 97
+    when ciphertext.between?(48, 57)
+      ciphertext = (((code - 48) + cipher_key) % 10) + 48
     else
       next
     end
