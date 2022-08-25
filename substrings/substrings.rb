8ab8270eb60e47ada1e801892d24d371ed0ed9e5
rscres <renatocrescenti@gmail.com>
2022-08-25 11:27:00 -0300

    Added downcase to string_arr

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -1,9 +1,8 @@
-
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings(string, dictionary)
   result = Hash.new(0)
-  string_arr = string.split
+  string_arr = string.downcase.split
   string_arr.each do |match|
     dictionary.each do |word|
       if match.include?(word)
